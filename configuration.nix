5ae2d1e96e9c6ae9d0bddf4bc5c23345859d11ca
Edwin Sarver <edwin.l.sarver@gmail.com>
2024-08-06 13:10:05 -0400

    organize services, add fwupd

--- configuration.nix
+++ configuration.nix
@@ -60,38 +60,40 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  services = {
+        fwupd.enable = true;
+        pipewire = {
+              enable = true;
+              alsa.enable = true;
+              alsa.support32Bit = true;
+              pulse.enable = true;
+              # If you want to use JACK applications, uncomment this
+              #jack.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+              # use the example session manager (no others are packaged yet so this is enabled by default,
+              # no need to redefine it in your config for now)
+              #media-session.enable = true;
+        };
+        # Enable CUPS to print documents.
+        printing.enable = true;
+        xserver = {
+            # Enable the X11 windowing system.
+            enable = true;
+            # Enable the GNOME Desktop Environment.
+            displayManager.gdm.enable = true;
+            desktopManager.gnome.enable = true;
+            # Configure keymap in X11
+            xkb = {
+                layout = "us";
+                variant = "";
+            };
+        };
   };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
