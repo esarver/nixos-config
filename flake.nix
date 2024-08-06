51b0d9cf8c3f64243f33bf2bbc9653c98129c6a5
Edwin Sarver <edwin.l.sarver@gmail.com>
2024-08-06 13:59:20 -0400

    add nixos-cosmic flake

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixos-cosmic = {
+        url = "github:lilyinstarlight/nixos-cosmic";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
         url = "github:nix-community/home-manager/release-24.05";
         inputs.nixpkgs.follows = "nixpkgs";
@@ -15,6 +19,12 @@
         esarverthinkpad = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
+                {
+                    nix.settings = {
+                        substituters = ["https://cosmic.cachix.org/"];
+                        trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+                    };
+                }
                 ./configuration.nix
                 home-manager.nixosModules.home-manager {
                     home-manager.useGlobalPkgs = true;
